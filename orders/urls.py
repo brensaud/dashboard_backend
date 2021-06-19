76ab08c0f6f8f7ae0f1c46d50d9328dba5454b62
Biren Saud <brensaud4@gmail.com>
2021-06-19 21:02:00 +0545

    orders app model and serializers and views created

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,9 @@
 
 from django.urls import path 
 
+from .views import OrderGenericAPIView
 
 urlpatterns = [
-
-    # path('products', ProductGenericAPIView.as_view()),
-    # path('products/<str:pk>', ProductGenericAPIView.as_view()),
+    path('orders', OrderGenericAPIView.as_view()),
+    path('orders/<str:pk>', OrderGenericAPIView.as_view()),
 ]
