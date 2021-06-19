7347f226c51c1667af6e72336d5f22f4e6ace6ca
Biren Saud <brensaud4@gmail.com>
2021-06-19 18:38:05 +0545

    permissions extracted of users for frontend

--- users/views.py
+++ users/views.py
@@ -62,10 +62,10 @@ class AuthenticatedUser(APIView):
     Permission_classes = [IsAuthenticated]
     
     def get(self, request):
-        serializer = UserSerializer(request.user, many=True)
-
+        data = UserSerializer(request.user).data
+        data['permissions'] = [p['name'] for p in data['role']['permissions']]
         return Response ({
-            'data': serializer.data
+            'data': data
         })
 
 
